30ac8f55580f1a048d4887df906b99a6028e7bec
fortplanta <106241509+fortplanta@users.noreply.github.com>
2026-08-31 11:55:03 +0200

    Register external Studio deployment

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,4 +5,7 @@ export default defineCliConfig({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? 'wvgj6m8r',
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production',
   },
+  deployment: {
+    appId: 'vtzxi91h76nful2l42kdixy1',
+  },
 })
